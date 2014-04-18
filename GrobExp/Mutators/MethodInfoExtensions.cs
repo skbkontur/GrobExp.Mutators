3df14bb97aaab7f037200f51590d850fc8f685d4
ichevdar <ichevdar@skbkontur.ru>
2014-04-18 13:15:27 +0600

    Забыл файлик

---
+++ GrobExp/Mutators/MethodInfoExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace GrobExp.Mutators
+{
+    public static class MethodInfoExtensions
+    {
+        public static bool IsExtension(this MethodInfo method)
+        {
+            return method.GetCustomAttributes(typeof(ExtensionAttribute), false).Any();
+        }
+
+        public static bool IsIndexerGetter(this MethodInfo method)
+        {
+            return method.Name == "get_Item";
+        }
+    }
+}

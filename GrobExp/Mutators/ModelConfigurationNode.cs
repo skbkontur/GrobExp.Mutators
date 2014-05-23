947f74488ed593bc605c7b9ae6ef026d6c11f535
denis boginich <denis.boginich@skbkontur.ru>
2014-05-23 18:54:53 +0600

    * fix bug

--- GrobExp/Mutators/ModelConfigurationNode.cs
+++ GrobExp/Mutators/ModelConfigurationNode.cs
@@ -138,7 +138,7 @@ namespace GrobExp.Mutators
             Expression result;
             if(!arrays.TryGetValue(RootType, out result))
                 return null;
-            return ExpressionEquivalenceChecker.Equivalent(result, Path, false, true) ? null : result;
+            return ExpressionEquivalenceChecker.Equivalent(Expression.Lambda(result, result.ExtractParameters()).ExtractPrimaryDependencies()[0].Body, Path, false, true) ? null : result;
         }
 
         public Dictionary<Type, Expression> GetArrays(bool cutTail)

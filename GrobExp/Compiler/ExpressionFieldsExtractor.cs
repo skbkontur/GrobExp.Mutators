0a9eb4e21cc796aabc51ab420a71aeae6930b3c2
unknown <boroka@k1506024.kontur>
2015-07-13 15:05:36 +0500

    Prepared interface for private methods replacement

--- GrobExp/Compiler/ExpressionFieldsExtractor.cs
+++ GrobExp/Compiler/ExpressionFieldsExtractor.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -13,7 +16,8 @@ namespace GrobExp.Compiler
 
             var expression = Visit(node.Expression);
 
-            if (expression != null && member.MemberType == MemberTypes.Field && (expression.Type.IsNestedPrivate || !((FieldInfo)member).Attributes.HasFlag(FieldAttributes.Public)))
+            if (expression != null && member.MemberType == MemberTypes.Field &&
+                (expression.Type.IsNestedPrivate || !((FieldInfo)member).Attributes.HasFlag(FieldAttributes.Public)))
             {
                 var extractor = FieldsExtractor.GetExtractor(member as FieldInfo);
                 if(expression.NodeType == ExpressionType.Convert)
@@ -22,6 +26,28 @@ namespace GrobExp.Compiler
             }
 
             return node.Update(expression);
+        }
+
+        protected override Expression VisitMethodCall(MethodCallExpression node)
+        {
+            if(node.Method.Attributes.HasFlag(MethodAttributes.Public))
+                return base.VisitMethodCall(node);
+
+            var arguments = new List<Expression>();
+            if (node.Object != null)
+                arguments.Add(Visit(node.Object));
+            arguments.AddRange(Visit(node.Arguments));
+
+            var methodDelegate = CreateDynamicMethod(node.Method);
+            var methodDelegateType = Extensions.GetDelegateType(arguments.Select(e => e.Type).ToArray(), node.Method.ReturnType);
+
+            return Expression.Call(Expression.Convert(Expression.Constant(methodDelegate), methodDelegateType),
+                methodDelegateType.GetMethod("Invoke"), arguments);
+        }
+
+        private static Delegate CreateDynamicMethod(MethodInfo method)
+        {
+            throw new NotImplementedException();
         }
     }
 }

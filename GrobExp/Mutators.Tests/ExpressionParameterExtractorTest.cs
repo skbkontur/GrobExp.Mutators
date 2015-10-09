d341bb53838f07d8b3a5791cec0b9cbcd8dc586f
unknown <anton92nd@k1205009.kontur>
2015-10-09 19:56:21 +0500

    fixed indentation with spaces

--- GrobExp/Mutators.Tests/ExpressionParameterExtractorTest.cs
+++ GrobExp/Mutators.Tests/ExpressionParameterExtractorTest.cs
@@ -7,51 +7,51 @@ using NUnit.Framework;
 
 namespace Mutators.Tests
 {
-	class ExpressionParameterExtractorTest : TestBase
-	{
-		private Expression<Func<object[], bool>> CompileLambda(Expression lambda)
-		{
-			var expressionsParameter = Expression.Parameter(typeof(object[]), "exprs");
-			object[] parameters;
-			return Expression.Lambda<Func<object[], bool>>(
-				new ExpressionParametersExtractor(expressionsParameter).ExtractParameters(lambda, out parameters),
-				expressionsParameter
-				);
-		}
+    class ExpressionParameterExtractorTest : TestBase
+    {
+        private Expression<Func<object[], bool>> CompileLambda(Expression lambda)
+        {
+            var expressionsParameter = Expression.Parameter(typeof(object[]), "exprs");
+            object[] parameters;
+            return Expression.Lambda<Func<object[], bool>>(
+                new ExpressionParametersExtractor(expressionsParameter).ExtractParameters(lambda, out parameters),
+                expressionsParameter
+                );
+        }
 
-		[Test]
-		public void TestExtractingParameter()
-		{
-			var expressionsParameter = Expression.Parameter(typeof(object[]), "exprs");
-			object[] parameters;
-			var lambdaToTest = ((Expression<Func<ValidatorsTest.TestData, bool>>)(z => z.S.Length > 1)).Body;
-			var compiledLambda = Expression.Lambda<Func<object[], bool>>(
-				new ExpressionParametersExtractor(expressionsParameter).ExtractParameters(lambdaToTest, out parameters),
-				expressionsParameter
-				).Compile();
-			Assert.IsTrue(compiledLambda.Invoke(new object[]{2}));
-		}
+        [Test]
+        public void TestExtractingParameter()
+        {
+            var expressionsParameter = Expression.Parameter(typeof(object[]), "exprs");
+            object[] parameters;
+            var lambdaToTest = ((Expression<Func<ValidatorsTest.TestData, bool>>)(z => z.S.Length > 1)).Body;
+            var compiledLambda = Expression.Lambda<Func<object[], bool>>(
+                new ExpressionParametersExtractor(expressionsParameter).ExtractParameters(lambdaToTest, out parameters),
+                expressionsParameter
+                ).Compile();
+            Assert.IsTrue(compiledLambda.Invoke(new object[] { 2 }));
+        }
 
-		[Test]
-		public void TestEquivalentAfterExtracting()
-		{
-			var lambda1 = ((Expression<Func<ValidatorsTest.TestData, bool>>)(z => z.S.Length > 1));
-			var lambda2 = ((Expression<Func<ValidatorsTest.TestData, bool>>)(z => z.D.S.Length > 1));
-			Assert.IsFalse(ExpressionEquivalenceChecker.Equivalent(lambda1, lambda2, true, false));
-			var extracted1 = CompileLambda(lambda1.Body);
-			var extracted2 = CompileLambda(lambda2.Body);
-			Assert.IsTrue(ExpressionEquivalenceChecker.Equivalent(extracted1, extracted2, true, false));
-		}
+        [Test]
+        public void TestEquivalentAfterExtracting()
+        {
+            var lambda1 = ((Expression<Func<ValidatorsTest.TestData, bool>>)(z => z.S.Length > 1));
+            var lambda2 = ((Expression<Func<ValidatorsTest.TestData, bool>>)(z => z.D.S.Length > 1));
+            Assert.IsFalse(ExpressionEquivalenceChecker.Equivalent(lambda1, lambda2, true, false));
+            var extracted1 = CompileLambda(lambda1.Body);
+            var extracted2 = CompileLambda(lambda2.Body);
+            Assert.IsTrue(ExpressionEquivalenceChecker.Equivalent(extracted1, extracted2, true, false));
+        }
 
-		[Test]
-		public void TestComplexExpressions()
-		{
-			var lambda1 = ((Expression<Func<ValidatorsTest.TestData, bool>>)(z => z.S.Length > 1 && z.A.S.Length < 0));
-			var lambda2 = ((Expression<Func<ValidatorsTest.TestData, bool>>)(z => z.D.S.Length > 1 && z.A.B[0].S.Length < 0));
-			Assert.IsFalse(ExpressionEquivalenceChecker.Equivalent(lambda1, lambda2, true, false));
-			var extracted1 = CompileLambda(lambda1.Body);
-			var extracted2 = CompileLambda(lambda2.Body);
-			Assert.IsTrue(ExpressionEquivalenceChecker.Equivalent(extracted1, extracted2, true, false));
-		}
-	}
+        [Test]
+        public void TestComplexExpressions()
+        {
+            var lambda1 = ((Expression<Func<ValidatorsTest.TestData, bool>>)(z => z.S.Length > 1 && z.A.S.Length < 0));
+            var lambda2 = ((Expression<Func<ValidatorsTest.TestData, bool>>)(z => z.D.S.Length > 1 && z.A.B[0].S.Length < 0));
+            Assert.IsFalse(ExpressionEquivalenceChecker.Equivalent(lambda1, lambda2, true, false));
+            var extracted1 = CompileLambda(lambda1.Body);
+            var extracted2 = CompileLambda(lambda2.Body);
+            Assert.IsTrue(ExpressionEquivalenceChecker.Equivalent(extracted1, extracted2, true, false));
+        }
+    }
 }
--- GrobExp/Mutators/Visitors/ExpressionParametersExtractor.cs
+++ GrobExp/Mutators/Visitors/ExpressionParametersExtractor.cs
@@ -5,42 +5,42 @@ using System.Linq.Expressions;
 
 namespace GrobExp.Mutators.Visitors
 {
-	public class ExpressionParametersExtractor : ExpressionVisitor
-	{
-		public ExpressionParametersExtractor(Expression parametersAccessor)
-		{
-			this.parametersAccessor = parametersAccessor;
-			paramsIndex = 0;
-		}
+    public class ExpressionParametersExtractor : ExpressionVisitor
+    {
+        public ExpressionParametersExtractor(Expression parametersAccessor)
+        {
+            this.parametersAccessor = parametersAccessor;
+            paramsIndex = 0;
+        }
 
-		public Expression ExtractParameters(Expression expression, out object[] parameters)
-		{
-			var result = Visit(expression);
-			parameters = new object[hashtable.Count];
-			foreach(DictionaryEntry entry in hashtable)
-			{
-				parameters[(int)entry.Value] = ((ExpressionWrapper)entry.Key).Expression;
-			}
-			return result;
-		}
+        public Expression ExtractParameters(Expression expression, out object[] parameters)
+        {
+            var result = Visit(expression);
+            parameters = new object[hashtable.Count];
+            foreach (DictionaryEntry entry in hashtable)
+            {
+                parameters[(int)entry.Value] = ((ExpressionWrapper)entry.Key).Expression;
+            }
+            return result;
+        }
 
-		public override Expression Visit(Expression node)
-		{
-			if(!node.IsLinkOfChain(true, true))
-			{
-				return base.Visit(node);
-			}
-			var key = new ExpressionWrapper(node, false);
-			var index = hashtable[key];
-			if(index == null)
-			{
-				hashtable[key] = index = paramsIndex++;
-			}
-			return Expression.Convert(Expression.ArrayIndex(parametersAccessor, Expression.Constant(index, typeof(int))), node.Type);
-		}
+        public override Expression Visit(Expression node)
+        {
+            if (!node.IsLinkOfChain(true, true))
+            {
+                return base.Visit(node);
+            }
+            var key = new ExpressionWrapper(node, false);
+            var index = hashtable[key];
+            if (index == null)
+            {
+                hashtable[key] = index = paramsIndex++;
+            }
+            return Expression.Convert(Expression.ArrayIndex(parametersAccessor, Expression.Constant(index, typeof(int))), node.Type);
+        }
 
-		private readonly Expression parametersAccessor;
-		private int paramsIndex;
-		private readonly Hashtable hashtable = new Hashtable();
-	}
+        private readonly Expression parametersAccessor;
+        private int paramsIndex;
+        private readonly Hashtable hashtable = new Hashtable();
+    }
 }

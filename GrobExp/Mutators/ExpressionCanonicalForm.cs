4c48630456c246e8eab28452ba33473532ad33b2
Anton Chaplygin <anton92nd@skbkontur.ru>
2015-11-23 19:41:35 +0500

    Mutators.csproj fix

--- GrobExp/Mutators/ExpressionCanonicalForm.cs
+++ GrobExp/Mutators/ExpressionCanonicalForm.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
 
-using GrobExp.Compiler;
 using GrobExp.Mutators.Visitors;
 
 namespace GrobExp.Mutators

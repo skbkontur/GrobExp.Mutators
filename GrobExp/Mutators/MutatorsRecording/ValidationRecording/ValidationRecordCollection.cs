cf218bedb8df003fefc6ea9711b4304d39123ffd
Карпова Ольга Дмитриевна <olga.karpova@skbkontur.ru>
2016-03-14 11:21:42 +0500

    отдельное дерево для хранения только валидаций с ошибками

--- GrobExp/Mutators/MutatorsRecording/ValidationRecording/ValidationRecordCollection.cs
+++ GrobExp/Mutators/MutatorsRecording/ValidationRecording/ValidationRecordCollection.cs
@@ -8,20 +8,28 @@ namespace GrobExp.Mutators.MutatorsRecording.ValidationRecording
         public ValidationRecordCollection()
         {
             validatorRecords = new List<RecordNode>();
+            errorValidationRecords = new List<RecordNode>();
         }
 
         public void AddValidatorToRecord(string validatorName)
         {
-            currentValidator = validatorRecords.FirstOrDefault(validator => validator.Name == validatorName);
-            if(currentValidator != null)
+            AddValidatorToRecord(validatorRecords, ref currentValidator, validatorName);
+            AddValidatorToRecord(errorValidationRecords, ref currentErrorValidator, validatorName);
+        }
+
+        private void AddValidatorToRecord(List<RecordNode> records, ref RecordNode validator, string validatorName)
+        {
+            validator = records.FirstOrDefault(v => v.Name == validatorName);
+            if (validator != null)
                 return;
-            currentValidator = new RecordNode(validatorName, "");
-            validatorRecords.Add(currentValidator);
+            validator = new RecordNode(validatorName, "");
+            records.Add(validator);
         }
 
         public void ResetCurrentValidator()
         {
             currentValidator = null;
+            currentErrorValidator = null;
         }
 
         public void RecordCompilingValidation(ValidationLogInfo validationInfo)
@@ -29,20 +37,33 @@ namespace GrobExp.Mutators.MutatorsRecording.ValidationRecording
             if(currentValidator == null) return;
             currentValidator.RecordCompilingExpression(new List<string> {validationInfo.Name, validationInfo.Condition}, "Ok");
             currentValidator.RecordCompilingExpression(new List<string> {validationInfo.Name, validationInfo.Condition}, "Error");
+
+            if(currentErrorValidator == null) return;
+            currentErrorValidator.RecordCompilingExpression(new List<string> {validationInfo.Name, validationInfo.Condition}, "Error");
         }
 
         public void RecordExecutingValidation(ValidationLogInfo validationInfo, string validationResult)
         {
             if(currentValidator != null)
                 currentValidator.RecordExecutingExpression(new List<string>{validationInfo.Name, validationInfo.Condition}, validationResult);
+
+            if(currentErrorValidator != null && validationResult == "Error")
+                currentErrorValidator.RecordExecutingExpression(new List<string>{validationInfo.Name, validationInfo.Condition}, validationResult);
         }
 
         public List<RecordNode> GetRecords()
         {
             return validatorRecords;
+        }
+
+        public List<RecordNode> GetErrorRecords()
+        {
+            return errorValidationRecords;
         } 
 
         private readonly List<RecordNode> validatorRecords;
         private RecordNode currentValidator;
+        private readonly List<RecordNode> errorValidationRecords;
+        private RecordNode currentErrorValidator;
     }
 }

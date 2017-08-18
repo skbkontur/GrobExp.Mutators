d40e9ac20d47b8173d27b48f2b763fcbff481351
msamoylenko <ghost1212@mail.ru>
2017-08-18 15:29:07 +0500

    Moved ModelConfigurationNode to original namespace to reduce compilation errors.

---
+++ GrobExp/Mutators/ModelConfigurationNode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+using GrobExp.Mutators.ModelConfiguration;
+
+namespace GrobExp.Mutators
+{
+    public class ModelConfigurationNode
+    {
+        public static ModelConfigurationNode CreateRoot(Type type)
+        {
+            return new ModelConfigurationNode(type, type, null, null, null, Expression.Parameter(type, type.Name));
+        }
+
+        internal ModelConfigurationNode(Type rootType, Type nodeType, ModelConfigurationNode root, ModelConfigurationNode parent, ModelConfigurationEdge edge, Expression path)
+        {
+            RootType = rootType;
+            NodeType = nodeType;
+            Root = root ?? this;
+            Parent = parent;
+            Edge = edge;
+            Path = path;
+            mutators = new List<KeyValuePair<Expression, MutatorConfiguration>>();
+            children = new Dictionary<ModelConfigurationEdge, ModelConfigurationNode>();
+        }
+
+        public override string ToString()
+        {
+            return this.ToPrettyString();
+        }
+
+        public Expression Path { get; private set; }
+        internal IEnumerable<ModelConfigurationNode> Children { get { return children.Values; } }
+        internal ICollection<KeyValuePair<Expression, MutatorConfiguration>> Mutators { get { return mutators; } }
+        public Type NodeType { get; private set; }
+        internal Type RootType { get; private set; }
+        internal ModelConfigurationNode Root { get; private set; }
+        internal ModelConfigurationNode Parent { get; private set; }
+        internal ModelConfigurationEdge Edge { get; private set; }
+        internal readonly List<KeyValuePair<Expression, MutatorConfiguration>> mutators;
+        internal readonly Dictionary<ModelConfigurationEdge, ModelConfigurationNode> children;
+    }
+}

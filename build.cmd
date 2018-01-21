42acec31f0e1e852541fd6ca8ba2287ed70a18eb
avk <Andrew.Kostousov@gmail.com>
2018-01-21 18:01:43 +0500

    update GrEmit, GroBuf deps and use nuget-package-refs

--- build.cmd
+++ build.cmd
@@ -3,6 +3,8 @@ set dotNetBasePath=%windir%\Microsoft.NET\Framework
 if exist %dotNetBasePath%64 set dotNetBasePath=%dotNetBasePath%64
 for /R %dotNetBasePath% %%i in (*msbuild.exe) do set msbuild=%%i
 
-set target=GrobExp\GrobExp.sln
+cd GrobExp
+..\nuget.exe restore
+%msbuild% /v:q /t:Rebuild /p:Configuration=Release /nodeReuse:false /maxcpucount GrobExp.sln
 
-%msbuild% /v:q /t:Rebuild /p:Configuration=Release /nodeReuse:false /maxcpucount %target%
+pause

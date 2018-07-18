a33a7f865452bde49e10333cd58473c462c9a52a
avk <Andrew.Kostousov@gmail.com>
2018-07-18 16:12:43 +0500

    fix nuget publish script

--- .publish-nuget-package.cmd
+++ .publish-nuget-package.cmd
@@ -1,19 +1,20 @@
 @echo off
 
 set SolutionName=GrobExp.Mutators
+set PackagedProjectName=Mutators
 
 rem reset current directory to the location of this script
 pushd "%~dp0"
 
-if exist "./%SolutionName%/bin" (
-    rd "./%SolutionName%/bin" /Q /S || exit /b 1
+if exist "./%PackagedProjectName%/bin" (
+    rd "./%PackagedProjectName%/bin" /Q /S || exit /b 1
 )
 
 dotnet build --force --no-incremental --configuration Release "./%SolutionName%.sln" || exit /b 1
 
 dotnet pack --no-build --configuration Release "./%SolutionName%.sln" || exit /b 1
 
-pushd "./%SolutionName%/bin/Release"
+pushd "./%PackagedProjectName%/bin/Release"
 dotnet nuget push *.nupkg -s https://nuget.org || exit /b 1
 popd
 

7271cf6a62cc0ed38a1b2d45fdbc4b7e2797e82a
Chaplygin Anton <anton92nd@skbkontur.ru>
2018-12-24 11:54:19 +0500

    Added english comments

--- Mutators/Visitors/InterfaceMemberResolver.cs
+++ Mutators/Visitors/InterfaceMemberResolver.cs
@@ -7,7 +7,7 @@ namespace GrobExp.Mutators.Visitors
     public class InterfaceMemberResolver : ExpressionVisitor
     {
         /// <summary>
-        ///     Удаляет касты к интерфейсам, чтобы упростить жизнь компилятору
+        ///     Removes redundant casts to interface in order to simplify expressions compiling
         /// </summary>
         protected override Expression VisitMember([NotNull] MemberExpression node)
         {
@@ -15,9 +15,13 @@ namespace GrobExp.Mutators.Visitors
                 return base.VisitMember(node);
             if (node.Expression?.NodeType != ExpressionType.Convert)
                 return base.VisitMember(node);
-            var expression = Visit(((UnaryExpression)node.Expression).Operand);
-            var members = expression.Type.GetMember(node.Member.Name);
-            return members.Length != 1 ? node.Update(expression) : Expression.MakeMemberAccess(expression, members[0]);
+
+            var expressionBeforeConvert = Visit(((UnaryExpression)node.Expression).Operand);
+
+            // Should be impossible to define two interface members having the same name
+            // Case when no members match by name should also be impossible
+            var members = expressionBeforeConvert.Type.GetMember(node.Member.Name);
+            return members.Length != 1 ? node.Update(expressionBeforeConvert) : Expression.MakeMemberAccess(expressionBeforeConvert, members[0]);
         }
     }
 }
